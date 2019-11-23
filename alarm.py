231c605b7c2f0481c4caf7f9be79dd367cbd94d0
Sandro Schwarz <sandroschwarz@wtnet.de>
2019-11-23 17:14:58 +0100

    Finalizing functionality

--- alarm.py
+++ alarm.py
@@ -1,12 +1,22 @@
+#!/bin/env python3
+
+# Written by Sandro Schwarz
+
 import logging as l # used for logging to file
 import socket as s # used to get machine FQDN (displayed in the logs)
 import datetime as d # for getting and formatting out own date display
 import smtplib as m # used for sending critical emails
-import time as t # used for waiting
+import time as t # used for idling
+import os as o # for checking if log folder exists and creating one in case it doesn't
+import configparser as c # for loading config from config.ini
 
 # initializing variables
 lastMailSent = ""
 lastLogEntry = ""
+
+# CHECK FOR LOG FOLDER
+if o.path.isdir("./logs") == False:
+    o.mkdir("./logs")
 
 # LOG CONFIG
 LOG_FILENAME = d.datetime.now().strftime("%d-%m-%Y_%H-%M-%S.log") # Log will have a timestamp like this for its name
@@ -16,70 +26,70 @@ LOG_TEXT = "{desc} usage is: {percent}% ({status})" # Setting out custom log mes
 LOG_DATEFORMAT = "%d.%m.%Y %H:%M:%S" # This will be our displayed timestamp in the log
 
 # EMAIL CONFIG
-port = 1025  # For running a local SMTP server on port 1025
-smtpServer = "localhost"
-message = "Subject: [CRITICAL] Hard Limit Warning!\n\nWarning, {desc} is at {spec}% and therefore over the hard limit of 90%."
-fromAdd = "alarm@localhost"
-toAdd = "admin@localhost"
+config = c.ConfigParser()
+config.read("config.ini")
+port = config["EMAIL"]["port"]
+smtpServer = config["EMAIL"]["smtpServer"]
+messageHead = config["EMAIL"]["messageHead"]
+messageBody = config["EMAIL"]["messageBody"]
+fromAdd = config["EMAIL"]["fromAdd"]
+toAdd = config["EMAIL"]["toAdd"]
+message = "Subject: {}\n\n{}".format(messageHead, messageBody)
+
 
 MACHINE_FQDN = s.getfqdn()
 MAP = {"fqdn": MACHINE_FQDN} # used for formatting log lines
 
-
+logCooldown = 1 # minute
+mailCooldown = 1 #hour
 
 # Basic log configuration, including log name, level of logging (set to DEBUG to get nominal stats as well)
 l.basicConfig(
     format=LOG_FORMAT,
     datefmt=LOG_DATEFORMAT,
     filename=LOG_PATH,
-    filemode='w',
+    filemode="w+",
     level=l.WARNING,
 )
 
 def sendEmailWarning(spec, desc):
     with m.SMTP(smtpServer, port) as server:
-        server.sendmail(fromAdd, toAdd, message.format(spec=spec, desc=desc))
+        server.sendmail(fromAdd, toAdd, message.format(spec=spec, desc=desc, pc=s.getfqdn()))
 
 # Main function for checking against soft and hard limits, logging accordingly and sending warnings
 def specStat(spec, desc):
     global lastMailSent
     global lastLogEntry
     status = ""
-    if 0 <= spec <= 80: 
+    if 0 <= spec <= 80: # check if percentage is nominal
         status = "Nominal"
-        if lastLogEntry == "":
-            print("Date of last log entry not found, writing one now and setting the var.")
-            l.info(LOG_TEXT.format(desc=desc, percent=spec, status=status), extra=MAP)
-            lastLogEntry = d.datetime.now()
-        elif lastLogEntry + d.timedelta(hours=12) < d.datetime.now():
-            l.info(LOG_TEXT.format(desc=desc, percent=spec, status=status), extra=MAP)
-            lastLogEntry = d.datetime.now()
-    elif 80 < spec < 90:
+    elif 80 < spec < 90: # check if percentage is in soft limit
         status = "Warning"
-        if lastLogEntry == "":
-            print("Date of last log entry not found, writing one now and setting the var.")
+        if lastLogEntry == "": # check if variable is empty, if empty, log and set variable accordingly
+            l.warning(LOG_TEXT.format(desc=desc, percent=spec, status=status), extra=MAP)
+            lastLogEntry = d.datetime.now() # update lastLogEntry variable if 
+        elif lastLogEntry + d.timedelta(minutes=logCooldown) < d.datetime.now(): # if last entry plus logCooldown is older than current time, then log
             l.warning(LOG_TEXT.format(desc=desc, percent=spec, status=status), extra=MAP)
             lastLogEntry = d.datetime.now()
-        elif lastLogEntry + d.timedelta(hours=12) < d.datetime.now():
-            l.warning(LOG_TEXT.format(desc=desc, percent=spec, status=status), extra=MAP)
-            lastLogEntry = d.datetime.now()
-    else:
+    else: # check if percentage is in hard limit (or even negative?)
         status = "Critical"
         if lastLogEntry == "":
-            print("Date of last log entry not found, writing one now and setting the var.")
             l.critical(LOG_TEXT.format(desc=desc, percent=spec, status=status), extra=MAP)
             lastLogEntry = d.datetime.now()
-        elif lastLogEntry + d.timedelta(hours=12) < d.datetime.now():
+        elif lastLogEntry + d.timedelta(minutes=logCooldown) < d.datetime.now():
             l.critical(LOG_TEXT.format(desc=desc, percent=spec, status=status), extra=MAP)
             lastLogEntry = d.datetime.now()
         try:
             if lastMailSent == "":
                 sendEmailWarning(spec, desc)
                 lastMailSent = d.datetime.now()
-            elif lastMailSent + d.timedelta(days=1) < d.datetime.now():
+            elif lastMailSent + d.timedelta(hours=mailCooldown) < d.datetime.now():
                 sendEmailWarning(spec, desc)
                 lastMailSent = d.datetime.now()
         except Exception as e:
             print("Couldn't send email:", e)
     output = LOG_TEXT.format(desc=desc, percent=spec, status=status)
     print(output)
+
+if __name__ == "__main__":
+    print("Sorry, this module is meant for importing only.")
--- monitoring.py
+++ monitoring.py
@@ -1,10 +1,19 @@
-#!/bin/env python
+#!/bin/env python3
+
+# Written by Sandro Schwarz
 
 import threading as t # import threading to run monitoring idependently
 import alarm as a # import own module (./alarm.py)
 import psutil as p # import psutil (pip install requirements.txt) to get hardware stats
 import os, time, platform # import built-in modules
+import argparse # for parsing flags like '-i'
 
+# Set up config parser, mainly used to check for interactive mode
+parser = argparse.ArgumentParser(description="Monitor system health and send emails in case of emergencies.")
+parser.add_argument("-i", action="store_true", default=False, help="Interactive mode. Set for watching stats indefinitely.", dest="boolean_switch")
+args = parser.parse_args()
+
+# Define a function to clear the screen
 def clear():
     if platform.system() == "Windows":
         os.system("cls")
@@ -13,17 +22,21 @@ def clear():
 
 maxPid = 500 # INI?
 
-while True:
-    monitorStats = {
-    "CPU": p.cpu_percent(),
-    "RAM": p.virtual_memory().percent,
-    "Disk": p.disk_usage("/").percent,
-    "PID": round(len(p.pids())/maxPid*100, 2) # converting amount of PIDs to percentage first
-    }
+if __name__ == "__main__":
+    while True:
+        # Define which stats should be monitored and how to find out it's percentage
+        monitorStats = {
+        "CPU": p.cpu_percent(),
+        "RAM": p.virtual_memory().percent,
+        "Disk": p.disk_usage("/").percent,
+        "PID": round(len(p.pids())/maxPid*100, 2) # converting amount of PIDs to percentage first
+        }
 
-    clear()
-    for i,j in monitorStats.items():
-        t.Thread(target=a.specStat(j,i)).start()
-        
-    time.sleep(.1)
+        clear()
+        for i,j in monitorStats.items():
+            t.Thread(target=a.specStat(j,i)).start() # start a thread that calls the alarm.py function specStat with percentage and description
+            
+        if args.boolean_switch == False: exit(1) # exit program if interactive mode is not set
+
+        time.sleep(1) # wait a second until next iteration
 
